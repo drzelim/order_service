70f0fb6d6533419a417a79be788e40408d96a191
Zelimkhan <kastiel85@mail.ru>
2024-09-14 21:27:07 +0300

    Complete L1.3

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
+use std::sync::{mpsc, Arc};
 use std::thread;
 
-fn get_num_cores() -> u32 {
-    let num_cores = thread::available_parallelism()
+fn get_num_cores() -> usize {
+    thread::available_parallelism()
         .map(|n| n.get())
-        .unwrap_or(1) as u32;
-    num_cores
+        .unwrap_or(1) as usize
 }
 
 fn main() {
-    let numbers_count: u32 = 10;
-    let numbers: Arc<Vec<u32>> = Arc::new((1..=numbers_count).collect());
+    let numbers_count: usize = 10;
+    let numbers: Arc<Vec<i32>> = Arc::new((1..=numbers_count as i32).collect());
 
     let mut handles = Vec::new();
     let num_cores = get_num_cores();
 
-    let num_threads = if num_cores > numbers_count { numbers_count} else { num_cores };
+    let num_threads = num_cores.min(numbers_count);
 
     let index = Arc::new(AtomicUsize::new(0));
+    let (tx , rx) = mpsc::channel();
 
     for _ in 0..num_threads {
         let numbers = Arc::clone(&numbers);
         let index = Arc::clone(&index);
+        let tx1 = tx.clone();
 
         let handle = thread::spawn(move || {
             loop {
@@ -31,16 +32,28 @@ fn main() {
                 if i >= numbers.len() {
                     break;
                 }
-
+                
                 let num = numbers[i];
-                println!("{}", num * num);
+                let square = num * num;
+                tx1.send(square).unwrap();
             }
         });
 
         handles.push(handle);
     }
 
+    drop(tx);
+
+    let mut result = 0;
+
+    for received in rx {
+        result += received;
+    }
+
     for handle in handles {
         handle.join().unwrap();
     }
+
+    println!("Sum: {result}");
+
 }
